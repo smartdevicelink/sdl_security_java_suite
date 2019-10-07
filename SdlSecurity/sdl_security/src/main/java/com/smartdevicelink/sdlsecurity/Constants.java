562cd23bdd95ac76b91cc1c429371eba403ff590
Bilal Alsharifi <bilal.alsharifi@gmail.com>
2019-10-07 13:15:35 -0400

    Make Constants class package private

--- SdlSecurity/sdl_security/src/main/java/com/smartdevicelink/sdlsecurity/Constants.java
+++ SdlSecurity/sdl_security/src/main/java/com/smartdevicelink/sdlsecurity/Constants.java
@@ -6,7 +6,7 @@ import java.util.List;
 /**
  * Created by Bilal Alsharifi && Bretty on 2019-09-25.
  */
-public class Constants {
+class Constants {
     final static String CERT_URL = "http://www.mocky.io/v2/5d9b565132000072002ae80f";
     final static List<String> MAKE_LIST = Collections.singletonList("Ford");
 }
