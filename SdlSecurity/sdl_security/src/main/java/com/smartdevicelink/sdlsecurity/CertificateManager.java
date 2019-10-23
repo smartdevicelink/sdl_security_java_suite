43a1ab6a73a3581675167d67f848c1b69d75a6ea
Bilal Alsharifi <bilal.alsharifi@gmail.com>
2019-10-23 15:00:37 -0400

    Update code comments

--- SdlSecurity/sdl_security/src/main/java/com/smartdevicelink/sdlsecurity/CertificateManager.java
+++ SdlSecurity/sdl_security/src/main/java/com/smartdevicelink/sdlsecurity/CertificateManager.java
@@ -69,7 +69,8 @@ class CertificateManager {
                     JSONObject jsonResponse = new JSONObject(response.toString());
                     JSONObject dataJsonObject = (JSONObject) jsonResponse.get("data");
                     String certStringBase64 = dataJsonObject.getString("certificate");
-                    // To use with Android pre 26, use byte[] certBuffer = android.util.Base64.decode(certStringBase64.getBytes(), android.util.Base64.DEFAULT);
+                    // If compiling library for use with Android SDK versions earlier than 26, use the android.utl.Base64 instead
+                    // byte[] certBuffer = android.util.Base64.decode(certStringBase64.getBytes(), android.util.Base64.DEFAULT);
                     byte[] certBuffer = Base64.getDecoder().decode(certStringBase64);
                     listener.onSuccess(certBuffer);
                 } catch (Exception e) {
