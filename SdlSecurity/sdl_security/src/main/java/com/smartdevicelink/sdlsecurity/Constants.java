98eab7b2d520c387049db7e892cdbe5840ffaefc
Henigan <rheniga1@MGC12Z921DLVCG.fbpld77.ford.com>
2020-10-26 14:44:31 -0400

    Move constants to java code and acces through cpp

--- SdlSecurity/sdl_security/src/main/java/com/smartdevicelink/sdlsecurity/Constants.java
+++ SdlSecurity/sdl_security/src/main/java/com/smartdevicelink/sdlsecurity/Constants.java
@@ -9,4 +9,11 @@ import java.util.List;
 class Constants {
     final static String CERT_URL = "http://www.mocky.io/v2/5d9b565132000072002ae80f";
     final static List<String> MAKE_LIST = Collections.singletonList("SDL");
+    final static int BUFFER_SIZE_MAX = 4096;
+    final static String CERT_PASS = "password"; // This needs to be changed to the actual certificate password
+    final static String CERT_ISSUER = "SDL"; // This needs to be changed to the actual certificate issuer
+
+    static {
+        System.loadLibrary("security");
+    }
 }
